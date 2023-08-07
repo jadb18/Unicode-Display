bb577d32fbbc2afc53697f70ad70c1363a49c721
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-08-07 01:25:42 -0400

    Initial Commit

---
+++ Unicode Binary/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Unicode Binary
+//
+//  Created by Jad Beydoun on 8/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unicode Binary/Unicode_BinaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unicode_BinaryApp.swift
+//  Unicode Binary
+//
+//  Created by Jad Beydoun on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Unicode_BinaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
